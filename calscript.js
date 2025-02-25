fffbca5095d4b1e7a2908dd6fb0314395d238d31
miraculous-shubham <2447011@kiit.ac.in>
2025-02-25 08:28:49 +0530

    Initial commit

---
+++ calscript.js
@@ -0,0 +1,128 @@
+let screen=document.querySelector("#p1")
+let btn7=document.querySelector("#btn7")
+btn7.onclick=()=>{
+    
+    let newtext =(btn7.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btn4=document.querySelector("#btn4");
+btn4.onclick=()=>{
+    
+    let newtext =(btn4.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btn1=document.querySelector("#btn1");
+btn1.onclick=()=>{
+    
+    let newtext =(btn1.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btn8=document.querySelector("#btn8");
+btn8.onclick=()=>{
+    
+    let newtext =(btn8.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btn5=document.querySelector("#btn5");
+btn5.onclick=()=>{
+    
+    let newtext =(btn5.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btn2=document.querySelector("#btn2")
+btn2.onclick=()=>{
+    
+    let newtext =(btn2.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btn0=document.querySelector("#btn0")
+btn0.onclick=()=>{
+    
+    let newtext =(btn0.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btn9=document.querySelector("#btn9")
+btn9.onclick=()=>{
+    
+    let newtext =(btn9.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btn6=document.querySelector("#btn6")
+btn6.onclick=()=>{
+    
+    let newtext =(btn6.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btn3=document.querySelector("#btn3")
+btn3.onclick=()=>{
+    
+    let newtext =(btn3.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btnequal=document.querySelector("#btnequal")
+btnequal.onclick=()=>{
+    
+    screen.textContent=(eval(screen.textContent));
+    
+
+}
+let btnx=document.querySelector("#btnx")
+btnx.onclick=()=>{
+    
+    let newtext =(btnx.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btnplus=document.querySelector("#btnplus")
+btnplus.onclick=()=>{
+    
+    let newtext =(btnplus.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btnminus=document.querySelector("#btnminus")
+btnminus.onclick=()=>{
+    
+    let newtext =(btnminus.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btndivide=document.querySelector("#btndivide")
+btndivide.onclick=()=>{
+    
+    let newtext =(btndivide.textContent);
+    screen.textContent+=(newtext);
+    
+
+}
+let btnc=document.querySelector("#btnc")
+btnc.onclick=()=>{
+    screen.textContent=(" ");
+    
+
+}
+
+ 
